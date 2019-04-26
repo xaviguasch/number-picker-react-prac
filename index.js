226df66e8b05fdb1e188a21c1735804d50445f01
xaviguasch <xaviguasch@gmail.com>
2019-04-26 16:13:05 +0200

    finished prac

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hehehewwwwhe')
+const getNum = () => {
+  return Math.floor(Math.random() * 10) + 1
+}
+
+const gif = 'https://media.giphy.com/media/rypyVNU547qrC/giphy.gif'
+
+class NumbPicker extends React.Component {
+  render() {
+    const num = getNum()
+    let msg
+    if (num === 7) {
+      msg = (
+        <div>
+          <h2>CONGRATS, YOU WIN!</h2>
+          <img src={gif} alt='' />
+        </div>
+      )
+    } else {
+      msg = <p>Sorry, you loose!</p>
+    }
+
+    return (
+      <div>
+        <h1>Your number is: {num}</h1>
+        {msg}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<NumbPicker />, document.getElementById('root'))
